7f727b0c6bb69236288c3a7a2902c389599ab54c
Yoshiaki Oshiro <himajin315@gmail.com>
2017-10-14 02:45:54 +0900

    Add fix_exif_rotation

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,6 +1,7 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
+  process :fix_exif_rotation
 
   if Rails.env.production?
     storage :fog
