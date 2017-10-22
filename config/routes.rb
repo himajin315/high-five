f606a3a8c078103519ac2e13faa898688f248b79
Yoshiaki Oshiro <himajin315@gmail.com>
2017-10-23 03:40:03 +0900

    Add route palm_information

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'high_five#index'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :users, only: [ :edit, :update, :destroy ]
+  resources :users, only: [ :edit, :update, :destroy ] do
+    resource :palm_information
+  end
 
   get '/entory', to: 'high_five#entory'
   get '/reading', to: 'high_five#reading'
