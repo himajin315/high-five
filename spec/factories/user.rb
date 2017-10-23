d88b1f546da97700a5893783e88a7fb59a290b3e
Yoshiaki Oshiro <himajin315@gmail.com>
2017-10-23 20:54:57 +0900

    Fix authenticate

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :user do
     sequence(:name) { |n| "User#{n}"}
     sequence(:email) { |n| "email#{n}@example.com"}
+    sequence(:password) { |n| "password"}
     provider  'facebook'
     sequence(:uid) { |n| "#{n}"}
 
