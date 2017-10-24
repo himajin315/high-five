048cad572f5495b018da82d0270c3a7eb3463601
Yoshiaki Oshiro <himajin315@gmail.com>
2017-10-25 02:04:46 +0900

    Modify reading page

--- app/controllers/high_five_controller.rb
+++ app/controllers/high_five_controller.rb
@@ -9,5 +9,12 @@ class HighFiveController < ApplicationController
 
   def reading
     authorize!
+
+    unread_ids = PalmInformation.unread.ids
+    @unread_users = User.where(id: PalmInformation.unread.ids).select(:id, :name, :updated_at).order(updated_at: :desc)
+    reread_ids = PalmInformation.reread.ids
+    @reread_users = User.where(id: PalmInformation.reread.ids).select(:id, :name, :updated_at).order(updated_at: :desc)
+    read_ids = PalmInformation.read.ids
+    @read_users = User.where(id: PalmInformation.read.ids).select(:id, :name, :updated_at).order(updated_at: :desc)
   end
 end
