4fb6a5dcb15cbe7a8dbfbdb911bfbd288ff2e0ff
Yoshiaki Oshiro <himajin315@gmail.com>
2017-11-10 02:32:27 +0900

    Add enum, feeling_length, feeling_slopek, knowledge_length, knowledge_slope, fate_slope

--- app/models/palm_information.rb
+++ app/models/palm_information.rb
@@ -7,7 +7,12 @@ class PalmInformation < ApplicationRecord
   validates :knowledge_slope,  numericality: { only_integer: true }, allow_nil: true
   validates :fate_slope,       numericality: { only_integer: true }, allow_nil: true
 
-  enum status: [:unread, :read, :reread]
+  enum status:           [:unread, :read, :reread]
+  enum feeling_length:   [:feeling_length_short, :feeling_length_normal, :feeling_length_long]
+  enum feeling_slope:    [:feeling_slope_middle, :feeling_slope_center, :feeling_slope_index]
+  enum knowledge_length: [:knowledge_length_short, :knowledge_length_normal, :knowledge_length_long]
+  enum knowledge_slope:  [:knowledge_slope_straight, :knowledge_slope_center, :knowledge_slope_down]
+  enum fate_slope:       [:fate_slope_left, :fate_slope_center, :fate_right]
 
   private
 
